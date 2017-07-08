5bec97d434d5a4d794ef1a336e92a90c4cbf6bdf
Matthew Gordon <laxredemption5525@gmail.com>
2017-07-08 14:30:20 -0600

    Comment out production configuration for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
 const debug = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool: debug ? 'inline-sourcemap' : false,
   entry: path.join(__dirname, '/src/js/index.js'),
   module: {
-    rules: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
       },
-    }, {
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader', options: {
-          sourceMap: true
-        }
-      }, {
-        loader: 'sass-loader', options: {
-          sourceMap: true
-        }
-      }]
-    }]
+      {
+        test: /\.scss$/,
+        // use: ExtractTextPlugin.extract({
+          // fallback: 'style-loader',
+          use: [
+            { loader: 'style-loader', options: { sourceMap: true } },
+            { loader: 'css-loader', options: { sourceMap: true } },
+            { loader: 'sass-loader', options: { sourceMap: true } }
+          ]
+        // })
+      }
+    ]
   },
   output: {
-    path: path.join(__dirname, '/src'),
+    path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
     publicPath: '/'
   },
@@ -36,4 +37,16 @@ module.exports = {
     historyApiFallback: true,
     contentBase: './src'
   }
+  // TODO: differ between production build and dev build
+
+  // plugins: [
+  //   new ExtractTextPlugin('styles.css'),
+  //   new webpack.optimize.OccurrenceOrderPlugin(),
+  //   new webpack.DefinePlugin({
+  //     'process.env': {
+  //       NODE_ENV: JSON.stringify('production')
+  //     }
+  //   }),
+  //   new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+  // ]
 }
